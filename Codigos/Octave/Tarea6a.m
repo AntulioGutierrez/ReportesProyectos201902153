93652c4291117705114a723686b2cae79c1f505c
AntulioGutierrez <3221751940801@ingenieria.usac.edu.gt>
2024-02-17 23:08:36 -0600

    mod1t6a

--- Codigos/Octave/Tarea6a.m
+++ Codigos/Octave/Tarea6a.m
@@ -1,5 +1,61 @@
 pkg load database
 conn = pq_connect(setdbopts('dbname','tareas','host','localhost','port','5432','user','postgres','password','admin'));
 
-N=pq_exec_params (conn, 'select * from tarea6Siii1;');%ver datos en la tabla
-disp(N.data);
+%N=pq_exec_params (conn, 'select * from estudiantes;');%ver datos en la tabla
+%disp(N.data);
+
+while true
+    disp('1. Agregar nuevo estudiante');
+    disp('2. Editar información de estudiante existente');
+    disp('3. Eliminar estudiante de la base de datos');
+    disp('4. Ver lista de estudiantes')
+    disp('5. Salir');
+    opcion = input('Elige una opción: ', 's');
+
+    switch opcion
+        case '1'
+            nombre = input('Introdusca el nombre del estudiante: ', 's');
+            edad = input('Introdusca la edad del estudiante: ', 's');
+            genero = input('Introdusca el género del estudiante: ', 's');
+            direccion = input('Introdusca la dirección del estudiante: ', 's');
+            registro = sprintf("INSERT INTO estudiantes (nombre, edad, genero, direccion) VALUES ('%s', '%s', '%s', '%s');", nombre, edad, genero, direccion);
+            [err, msg] = lasterr();
+            if isempty(err)
+                pq_exec_params(conn, registro);
+                disp('Estudiante agregado exitosamente.');
+            else
+                disp(['Error: ' msg]);
+            end
+        case '2'
+            id = input('Introduce el ID del estudiante a editar: ', 's');
+            nombre = input('Introduce el nuevo nombre del estudiante: ', 's');
+            edad = input('Introduce la nueva edad del estudiante: ', 's');
+            genero = input('Introduce el nuevo género del estudiante: ', 's');
+            direccion = input('Introduce la nueva dirección del estudiante: ', 's');
+            consulta = sprintf("UPDATE estudiantes SET nombre = '%s', edad = '%s', genero = '%s', direccion = '%s' WHERE id = %s;", nombre, edad, genero, direccion, id);
+            [err, msg] = lasterr();
+            if isempty(err)
+                pq_exec_params(conn, consulta);
+                disp('Información del estudiante actualizada exitosamente.');
+            else
+                disp(['Error: ' msg]);
+            end
+        case '3'
+            id = input('Introduce el ID del estudiante a eliminar: ', 's');
+            consulta = sprintf("DELETE FROM estudiantes WHERE id = %s;", id);
+            [err, msg] = lasterr();
+            if isempty(err)
+                pq_exec_params(conn, consulta);
+                disp('Estudiante eliminado exitosamente.');
+            else
+                disp(['Error: ' msg]);
+            end
+        case '4'
+            N=pq_exec_params (conn, 'select * from estudiantes;');
+            disp(N.data);
+        case '5'
+            break;
+        otherwise
+            disp('Opción no válida');
+    end
+end
--- Codigos/Sql/tarea6Siii1.sql
+++
@@ -1,6 +0,0 @@
---drop table tarea6Siii1;
---create table tarea6Siii1 (nombrem varchar (50), edad int not null, genero varchar (10), direccion varchar (100));
---delete from tarea6Siii1;
---insert into tarea6Siii1 values ('Antulio', 24, 'Masculino', 'zona 21');
-select * from tarea6Siii1;
---select * from tare6;
