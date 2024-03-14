3cb4a862863e94a9f6f1685889efd14ff7e3bd33
AntulioGutierrez <3221751940801@ingenieria.usac.edu.gt>
2024-03-14 07:25:18 -0600

    tarea9

--- Codigos/Tarea8/proyecto/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.store, name='tienda'),
-    path('cart/', views.cart, name='carrito'),
-    path('checkout/', views.checkout, name='pago'),
-]
